54f60527428e876ac844c3a8ce38b2b10d01e01f
Heba-Magdyy <hmagdy666@gmail.com>
2024-08-19 19:42:23 +0300

    post request

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const app = express();
 const port = process.env.PORT;
 app.listen(port , ()=>console.log(`Server is running on port ${port} `))
 
+app.use(express.json())
 
 //Get request
 app.get("/" , (req, res) => {
     res.status(200).json({"msg" : "Get Request"})
+} )
+
+
+//Post request
+app.post("/" , (req, res) => {
+    res.status(201).json({"msg" : "Post Request"})
+    console.log(req.body)
 } )
